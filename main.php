10638f6f5c4b98815c12dac293aded8384535101
YasenAbdelghany <Yasen@Yasen-PC.citco.hq>
2015-09-20 12:44:00 +0200

    add main page

---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+require_once("_inc/header.php");
+?>
+			<!-- start: BREADCRUMB -->
+			<div class="row">
+				<div class="col-md-12">
+					<ol class="breadcrumb">
+						<li>
+							<a href="#">
+								Dashboard
+							</a>
+						</li>
+						<li class="active">
+							Home Page
+						</li>
+					</ol>
+				</div>
+			</div>
+			<!-- end: BREADCRUMB -->
+			<!-- start: PAGE CONTENT -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel panel-white">
+						<div class="panel-body">
+							Your content here
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- end: PAGE CONTENT-->
+		</div>
+		<div class="subviews">
+			<div class="subviews-container"></div>
+		</div>
+	</div>
+	<!-- end: PAGE -->
+</div>
+
+<!-- end: MAIN CONTAINER -->
+<?php
+require_once("_inc/footer.php");
+?>
